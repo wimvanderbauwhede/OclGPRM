332171a5e27a0d14acafd17aed3e3f8680c57bf4
Wim Vanderbauwhede <Wim.Vanderbauwhede@gmail.com>
2013-04-25 17:54:28 +0100

    nm

---
+++ src/timing.cc
@@ -0,0 +1,10 @@
+#include "timing.h"
+
+double wsecond() {
+    struct timeval sampletime;
+    double         time;
+
+    gettimeofday( &sampletime, NULL );
+    time = sampletime.tv_sec + (sampletime.tv_usec / 1000000.0);
+    return( time*1000.0 ); // return time in ms
+}
